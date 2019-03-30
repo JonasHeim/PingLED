8c46fe094c5400291f20573392a710ec6d0dfe0e
JonasHeim <heimjonas@yahoo.de>
2019-03-30 23:14:24 +0100

    Added Wi-Fi task and configuration for AccessPoint. Added Wi-Fi event callback.

--- pingLED/user/user_main.c
+++ pingLED/user/user_main.c
@@ -29,6 +29,38 @@
 #include "TM1637.h"
 
 #define GPIO_RESET_BTN 5 	/* GPIO for external reset button */
+#define WAIT_MS_DISPLAY_TEST	( 200U )	//Waiting time in ms for display test routine
+
+/* WI-FI configuration */
+#define ACCESS_POINT_SSID		( "pingLED")
+#define ACCESS_POINT_PASSKEY	( "password")
+
+/******************************************************************************
+ * FunctionName : wifi_handle_event_cb
+ * Description  : entry of user application, init user function here
+ * Parameters   : System_Event_t*	Event that caused the callback
+ * Returns      : none
+*******************************************************************************/
+void wifi_handle_event_cb(System_Event_t* event)
+{
+	switch(event->event_id)
+	{
+		case EVENT_SOFTAPMODE_STACONNECTED:
+			printf("Client with MAC %X:%X:%X:%X:%X:%X connected.\r\n",
+				event->event_info.sta_connected.mac[0],	event->event_info.sta_connected.mac[1],
+				event->event_info.sta_connected.mac[2], event->event_info.sta_connected.mac[3],
+				event->event_info.sta_connected.mac[4], event->event_info.sta_connected.mac[5]);
+			break;
+		case EVENT_SOFTAPMODE_STADISCONNECTED:
+			printf("Client with MAC %X:%X:%X:%X:%X:%X disconnected\r\n",
+			event->event_info.sta_disconnected.mac[0],	event->event_info.sta_disconnected.mac[1],
+			event->event_info.sta_disconnected.mac[2], event->event_info.sta_disconnected.mac[3],
+			event->event_info.sta_disconnected.mac[4], event->event_info.sta_disconnected.mac[5]);
+			break;
+		default:
+			break;
+	}
+}
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -42,7 +74,6 @@
  * Parameters   : none
  * Returns      : rf cal sector
 *******************************************************************************/
-
 uint32 user_rf_cal_sector_set(void)
 {
     flash_size_map size_map = system_get_flash_size_map();
@@ -83,6 +114,12 @@ uint32 user_rf_cal_sector_set(void)
     return rf_cal_sec;
 }
 
+/******************************************************************************
+ * FunctionName : task_display
+ * Description  : RTOS task that handles the I2C 7-Segment display
+ * Parameters   : void* Task Parameters
+ * Returns      : none
+*******************************************************************************/
 void task_display(void *pvParameters) 
 { 
 	printf("I2C_Task: Init I2C...\r\n"); 
@@ -91,58 +128,57 @@ void task_display(void *pvParameters)
 	tm1637_set_brightness(0x50);
 	uint8_t i = 0;
 	while (1) { 
-		printf("I2C_Task\r\n");
-
+		/* run display test pattern */
 		for(i = 0; i < 4; i++)
 		{
 
 			tm1637_display_char(tm1637_map_char('0'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('1'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('2'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('3'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('4'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('5'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('6'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('7'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('8'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('9'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('A'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('B'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('C'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('D'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('E'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_display_char(tm1637_map_char('F'), i);
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(WAIT_MS_DISPLAY_TEST / portTICK_RATE_MS);
 
 			tm1637_clear();
 		}
@@ -156,7 +192,13 @@ typedef enum {
 	RESET_STATE_TRIGGERED
 } RESET_STATE_t;
 
-void reset_task(void *pvParameters) 
+/******************************************************************************
+ * FunctionName : task_reset
+ * Description  : RTOS task that monitors the RESET button and triggers a reset
+ * Parameters   : void* Task Parameters
+ * Returns      : none
+*******************************************************************************/
+void task_reset(void *pvParameters) 
 { 
 	
 	uint8_t reset_state = RESET_STATE_IDLE;
@@ -174,8 +216,6 @@ void reset_task(void *pvParameters)
 
 		/* sample button level */
 		btn_current_level = GPIO_INPUT_GET(GPIO_RESET_BTN);
-
-		printf("Reset button on level: %d\r\n", btn_current_level);
 
 		switch (reset_state)
 		{
@@ -236,6 +276,118 @@ void reset_task(void *pvParameters)
 }
 
 /******************************************************************************
+ * FunctionName : task_wireless
+ * Description  : RTOS task for WIFI initialization and management
+ * Parameters   : void* Task Parameters
+ * Returns      : none
+*******************************************************************************/
+void task_wireless(void *pvParameters)
+{	
+
+    uint8 ssid[32];         /**< SSID of ESP8266 soft-AP */
+    uint8 password[64];     /**< Password of ESP8266 soft-AP */
+    uint8 ssid_len;         /**< Length of SSID. If softap_config.ssid_len==0, check the SSID until there is a termination character; otherwise, set the SSID length according to softap_config.ssid_len. */
+    uint8 channel;          /**< Channel of ESP8266 soft-AP */
+    AUTH_MODE authmode;     /**< Auth mode of ESP8266 soft-AP. Do not support AUTH_WEP in soft-AP mode */
+    uint8 ssid_hidden;      /**< Broadcast SSID or not, default 0, broadcast the SSID */
+    uint8 max_connection;   /**< Max number of stations allowed to connect in, default 4, max 4 */
+    uint16 beacon_interval; /**< Beacon interval, 100 ~ 60000 ms, default 100 */
+
+	static struct softap_config config_ap = 
+	{
+		.ssid = ACCESS_POINT_SSID,
+		.ssid_len = 0,
+		.password = ACCESS_POINT_PASSKEY,
+		.channel = 1,
+		.authmode = AUTH_WPA_WPA2_PSK,
+		.ssid_hidden = 0,	//broadcast SSID
+		.max_connection = 1,
+		.beacon_interval = 100
+	};
+
+	static struct ip_info ip_ap;
+	IP4_ADDR(&ip_ap.ip, 192, 168, 1, 1);
+	IP4_ADDR(&ip_ap.gw, 192, 168, 1, 1);
+	IP4_ADDR(&ip_ap.netmask, 255, 255, 255, 0);
+
+	static struct dhcps_lease dhcp_ap = 
+	{
+		.enable = true,	
+	};
+	IP4_ADDR(&dhcp_ap.start_ip, 192, 168, 1, 2);
+	IP4_ADDR(&dhcp_ap.end_ip, 192, 168, 1, 10);
+
+
+	printf("In task %s\r\n", __func__);
+
+	if(!wifi_set_opmode(STATIONAP_MODE))
+	{
+		/* could not set operation mode */
+		printf("Error in %s - Could not set operation mode!\r\n", __func__);
+		vTaskDelete(NULL); 
+	}
+
+	printf("##### 0 #####\r\n");
+
+	if(!wifi_softap_set_config(&config_ap))
+	{
+		/* could not set station configuration */
+		printf("Error in %s - Could not set access point configuration!\r\n", __func__);
+		vTaskDelete(NULL); 
+	}
+
+	printf("##### 1 #####\r\n");
+
+	/* set access point IP and DHCP configuration */
+	if(wifi_softap_dhcps_stop())
+	{
+		printf("##### 2 #####\r\n");
+		if(!wifi_set_ip_info(SOFTAP_IF, &ip_ap))
+		{
+			/* could not set station ip configuration */
+			printf("Error in %s - Could not set access point ip configuration!\r\n", __func__);
+			vTaskDelete(NULL); 
+		}
+
+		printf("##### 3 #####\r\n");
+
+		if(!wifi_softap_set_dhcps_lease(&dhcp_ap))
+		{
+			/* could not set access point DHCP configuration */
+			printf("Error in %s - Could not set access point DHCP configuration!\r\n", __func__);
+			vTaskDelete(NULL); 
+		}
+
+		printf("##### 4 #####\r\n");
+ 
+		if(!wifi_softap_dhcps_start())
+		{
+			/* could not start access point DHCP */
+			printf("Error in %s - Could not start access point DHCP!\r\n", __func__);
+			vTaskDelete(NULL); 
+		}
+
+		printf("##### 5 #####\r\n");
+	}
+	else
+	{
+		/* could not stop access point DHCP */
+		printf("Error in %s - Could not stop access point DHCP!\r\n", __func__);
+		vTaskDelete(NULL); 
+	}
+
+	/* register callback for WiFi events */
+	if(!wifi_set_event_handler_cb(wifi_handle_event_cb))
+	{
+		/* could not set WiFi event callback */
+		printf("Error in %s - Could not set Wi-Fi event callback!\r\n", __func__);
+		vTaskDelete(NULL); 
+	}
+
+	vTaskDelete(NULL); 
+}
+
+/******************************************************************************
  * FunctionName : user_init
  * Description  : entry of user application, init user function here
  * Parameters   : none
@@ -248,5 +400,7 @@ user_init(void)
 	os_delay_us(10000);
 	printf("SDK version:%s\n", system_get_sdk_version());
 	xTaskCreate(task_display, "Seven Segment Display Task", 256, NULL, 2, NULL); 
-    xTaskCreate(reset_task, "Reset Task", 256, NULL, 2, NULL);
+    xTaskCreate(task_reset, "Reset Task", 256, NULL, 2, NULL);
+	xTaskCreate(task_wireless, "Wi-Fi Management Task", 512, NULL, 2, NULL);
 }
+
